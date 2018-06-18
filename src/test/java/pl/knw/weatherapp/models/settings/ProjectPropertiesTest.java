3332ffe97b0ecef0c342cb474c69d7fd8e8a1485
tpazera <tomasz.pazera96@gmail.com>
2018-06-19 01:43:59 +0200

    Add 3 tests

--- src/test/java/pl/knw/weatherapp/models/settings/ProjectPropertiesTest.java
+++ src/test/java/pl/knw/weatherapp/models/settings/ProjectPropertiesTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
+import static pl.knw.weatherapp.models.settings.ProjectProperties.PROTOTYPE;
 
 @RunWith(JUnit4.class)
 public class ProjectPropertiesTest {
@@ -48,6 +49,44 @@ public class ProjectPropertiesTest {
 
         Properties properties = new ProjectProperties();
         assertNull(properties.remove(name));
+    }
+
+    @Test
+    public void testPrototypePut() {
+        Properties prototype = new ProjectProperties();
+        Properties properties = new ProjectProperties();
+        assertNull(properties.put(PROTOTYPE, prototype));
+        assertTrue(properties.has(PROTOTYPE));
+    }
+
+    @Test
+    public void testPrototypeHas() {
+        String name1 = "name1";
+        String name2 = "name2";
+        String value = "value";
+
+        Properties prototype = new ProjectProperties();
+        assertNull(prototype.put(name1, value));
+
+        Properties properties = new ProjectProperties();
+        assertNull(properties.put(PROTOTYPE, prototype));
+        assertTrue(properties.has(name1));
+        assertFalse(properties.has(name2));
+    }
+
+    @Test
+    public void testPrototypeGet() {
+        String name1 = "name1";
+        String name2 = "name2";
+        String value = "value";
+
+        Properties prototype = new ProjectProperties();
+        assertNull(prototype.put(name1, value));
+
+        ProjectProperties properties = new ProjectProperties();
+        assertNull(properties.put(PROTOTYPE, prototype));
+        assertEquals(value, properties.get(name1));
+        assertNull(properties.get(name2));
     }
 
 }
